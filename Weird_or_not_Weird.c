83c321fd0ddcf432f0843f18c5dc233c84d75f6d
nikhitadasari26 <147791798+nikhitadasari26@users.noreply.github.com>
2023-11-10 20:06:50 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     if(n%2==1)     {         printf("weird");
        }
        else
        {
            printf("not weird");
        }
    }

---
+++ Weird_or_not_Weird.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n%2==1)
+    {
+        printf("weird");
+        
+    }
+    else
+    {
+        printf("not weird");
+    }
+}
